9d9694ae32460d22620dd3e0b5c4925f96c3e27c
ZacHolme7 <zacholme@gmail.com>
2021-08-26 15:06:21 -0500

    first commit

---
+++ static/js/main.js
@@ -0,0 +1,32 @@
+// Gets the row and the cols of each cell on click
+const table = document.querySelector('.main-table');
+const rows = document.querySelectorAll('tr');
+const rowsArray = Array.from(rows);
+var rowIndex = 0
+var columns = 0
+var columnIndex = 0
+
+table.addEventListener('click', (event) => {
+  rowIndex = rowsArray.findIndex(row => row.contains(event.target));
+  columns = Array.from(rowsArray[rowIndex].querySelectorAll('td'));
+  columnIndex = columns.findIndex(column => column == event.target);
+})
+// ----------------------------------
+
+// Creates the starting grid and populates it will all 0's
+grid = []
+for(var i = 0; i < 75; i++){
+	grid.push(new Array(125).fill(0))
+}
+// -------------------------------------
+
+window.toggleTable = function (event) {
+	if(event.target.tagName == "TD"){
+		event.target.style.backgroundColor = "black";
+		grid[rowIndex][columnIndex] = 1
+  }
+}
+
+
+
+
