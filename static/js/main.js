9aa5eabe11b07a0ecbc27d353343a13ed81fd043
ZacHolme7 <zacholme@gmail.com>
2021-08-27 09:29:09 -0500

    update

--- static/js/main.js
+++ static/js/main.js
@@ -11,20 +11,35 @@ table.addEventListener('click', (event) => {
   columns = Array.from(rowsArray[rowIndex].querySelectorAll('td'));
   columnIndex = columns.findIndex(column => column == event.target);
 })
-// ----------------------------------
 
 // Creates the starting grid and populates it will all 0's
 grid = []
 for(var i = 0; i < 75; i++){
 	grid.push(new Array(125).fill(0))
 }
-// -------------------------------------
 
+// sets the cells, mainly just for the starting screen
 window.toggleTable = function (event) {
 	if(event.target.tagName == "TD"){
 		event.target.style.backgroundColor = "black";
 		grid[rowIndex][columnIndex] = 1
   }
+}
+
+
+
+function execute(){
+	/*
+	 *while it is running, ex while not paused/stop
+	 update the grid
+	 render that grid
+	 continue the process
+	 */
+	
+}
+
+function updateGrid(grid){
+
 }
 
 
