62d8c2116a9cd6337fbb4c14619747f603ca710a
ZacHolme7 <zacholme@gmail.com>
2021-08-30 19:37:45 -0500

    got reset and basic functionality working

--- static/js/main.js
+++ static/js/main.js
@@ -14,8 +14,10 @@ table.addEventListener('click', (event) => {
 
 // Creates the starting grid and populates it will all 0's
 grid = []
+gridOutput = []
 for(var i = 0; i < 75; i++){
 	grid.push(new Array(125).fill(0))
+	gridOutput.push(new Array(125).fill(0))
 }
 
 // sets the cells, mainly just for the starting screen
@@ -23,24 +25,84 @@ window.toggleTable = function (event) {
 	if(event.target.tagName == "TD"){
 		event.target.style.backgroundColor = "black";
 		grid[rowIndex][columnIndex] = 1
+		gridOutput[rowIndex][columnIndex] = 1
   }
 }
 
+// Resets the grid, sets all background colors to starting and resets grid to off
+function resetGrid(){
+	for(var i = 0, row; row = table.rows[i]; i++){
+		for(var j = 0, col; col = row.cells[j]; j++){
+			col.style.backgroundColor = "#c7c7c7";
+			grid[i][j] = 0
+			gridOutput[i][j] = 0
+		}
+	}	
+}
 
 
+// Rules for GOL
+// ----------------
+// when cell is alive (1)
+// 	less than 2 neighbors -> dies
+// 	more than 3 neightbors -> dies
+// 	else -> lives
+// when cell is dead (0)
+// 	3 neighbors -> becomes alive
+
+
+// main function loop to update, redraw, handle pause logic
 function execute(){
-	/*
-	 *while it is running, ex while not paused/stop
-	 update the grid
-	 render that grid
-	 continue the process
-	 */
-	
+	// store and render output
+	for(var i = 0; i < 75; i++){
+		for(var j = 0; j < 125; j++){
+			gridOutput[i][j] = grid[i][j]
+		}
+	}
+
+	// call to a function to render the grid onto the screen
+	renderGrid()
+	// update to the next grid, call the update grid function or just preform operation here
+	updateGrid()
 }
 
-function updateGrid(grid){
+// renders the current grid state to the screen by changing the background colors appropriately
+function renderGrid(){
+	for(var i = 0, row; row = table.rows[i]; i++){
+		for(var j = 0, col; col = row.cells[j]; j++){
+			if(gridOutput[i][j]){
+				col.style.backgroundColor = "black";
+			} else {
+				col.style.backgroundColor = "#c7c7c7";
+			}
+		}
+	}	
+}
+
+// updates the grid to the next generation
+function updateGrid(){
+	for(var i = 1; i < 75; i++){
+		for(var j = 1; j < 125; j++){
+			var neighbors = gridOutput[i-1][j-1] + gridOutput[i][j-1] + gridOutput[i+1][j-1] +
+					gridOutput[i-1][j]   + 			    gridOutput[i+1][j] + 
+					gridOutput[i-1][j+1] + gridOutput[i][j+1] + gridOutput[i+1][j+1]
+
+			// if cell alive, stays alive only if 2 or 3 neighbors
+			// if cell dead, becomes alive if it has 3 neighbors
+			if(gridOutput[i][j]){
+				grid[i][j] = neighbors == 2 || neighbors == 3
+			} else {
+				grid[i][j] = neighbors == 3
+			}
+		}
+	}
+}
+
+
+function pauseExecution(){
 
 }
+
 
 
 
